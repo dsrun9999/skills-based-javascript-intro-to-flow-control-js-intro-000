ab8fc993eb1a2b8cf1adf1925b2a866215285c73
Denny Srun <dennysrun@gmail.com>
2021-04-01 07:22:19 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -31,8 +31,8 @@ function switchAge(age) {
   switch(age) {
     case "age >= 13 && age <= 19":
         return; "You are a teenager"
-    
-      default: 
+
+      default:
         return "You have an age"
   }
 }
