0cb9e808cb9777d223cc71aaba665cf34befaaec
Denny Srun <dennysrun@gmail.com>
2021-04-01 06:55:35 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -22,7 +22,7 @@ function ageChecker(age) {
   }
 
 function ternaryTeenager(age) {
-  return ?return ('You are a teenager!' : 'You are not a teenager');
+  return ? ('You are a teenager!' : 'You are not a teenager');
 
 
 }
